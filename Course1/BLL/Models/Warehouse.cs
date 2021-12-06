0ae8151b2c504e8d14731369c4026f640e7b284a
VyacheslavVlasoff <slava-vlasov-99@mail.ru>
2021-12-06 17:02:00 +0300

    Create Warehouse.cs

---
+++ Course1/BLL/Models/Warehouse.cs
@@ -0,0 +1,25 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BLL.Models
+{
+    public partial class Warehouse
+    {
+        public int Id { get; set; }
+
+        public string Addres { get; set; }
+
+        public Warehouse() { }
+        public Warehouse(WAREHOUSE w)
+        {
+            Id = w.ID;
+            Addres = w.ADDRESS;
+        }
+    }
+}
