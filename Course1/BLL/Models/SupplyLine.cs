7c5ca7714e5d29fb17dffe8b8dcd3539ac081fd8
VyacheslavVlasoff <slava-vlasov-99@mail.ru>
2021-12-06 17:02:15 +0300

    Create SupplyLine.cs

---
+++ Course1/BLL/Models/SupplyLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DAL;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Models
+{
+    public partial class SupplyLine
+    {
+        public int SupplyId { get; set; }
+
+        public int CommondityId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public SupplyLine() { }
+        public SupplyLine(SupplyLine s)
+        {
+            SupplyId = s.SupplyId;
+            CommondityId = s.CommondityId;
+            Quantity = s.Quantity;
+            Cost = s.Cost;
+        }
+    }
+}
